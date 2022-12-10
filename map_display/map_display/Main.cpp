90c51a6e3a09c6d7ad39319126dd18287c5f8f47
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2022-12-10 15:11:15 +0900

    ファイル追加

---
+++ map_display/map_display/Main.cpp
@@ -0,0 +1,33 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.5
+#include"display_manager.h"
+void Main()
+{
+
+	screen_resizer display(1200,1000);
+	double scalling_x,scalling_y;
+	double scall;
+	//Scene::SetResizeMode(ResizeMode::Keep);
+	//シーンサイズ固定 今回はこれは使わずにやる
+	Window::SetStyle(WindowStyle::Sizable);
+	//ウィンドウを自由に大きさ変えれるやつ
+	//Rect{座標ｘ,座標y,大きさx,大きさy}.draw
+
+	while (System::Update())
+	{
+		display.resizer();
+		//Rect{300,300, 200,300 }.draw();
+		scalling_x = Scene::Size().x / 1200.0;
+		scalling_y= Scene::Size().y / 1000.0;
+		scall=scalling_x < scalling_y ? scalling_x : scalling_y;
+
+
+		//RectF{display.pointer().x,0,100,100}.draw();
+		RectF{ display.pointer().x,display.pointer().y, 1200.0 * scall,1000.0 * scall}.draw(Palette::Aqua);
+		ClearPrint();
+		Print << Scene::Size().x;
+		Print << Scene::Size().y;
+		Print << Scene::Height();
+		Print << Scene::Width();
+		Print << 1200 * scall;
+	}
+}
---
+++ map_display/map_display/display_manager.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+/*
+開発開始　12月６日
+開発者　藤本涼
+opensiv用の画面の縮小拡大を自動的に行うもの
+*/
+
+
+
+class screen_resizer {
+private:
+	int x=0,y=0;
+	Vec2 pos;
+	Vec2 point;
+	Vec2 Op;
+	Vec2 scalling;
+	double scall;
+public:
+	screen_resizer(double x,double y ) {
+		pos.x = x;
+		pos.y = y;
+
+	}
+	/*仮想スクリーンの大きさを指定する*/
+
+
+	void resizer() {
+		scalling.x = Scene::Size().x / pos.x;
+		scalling.y = Scene::Size().y / pos.y;
+		scall = scalling.x < scalling.y ? scalling.x : scalling.y;
+
+		Op.x = (Scene::Size().x - 1200.0 * scall) * 0.5;
+		Op.y = (Scene::Size().y - pos.y * scall) * 0.5;
+	}
+
+	Vec2  pointer(Vec2& p) {//左下からの座標にします！！
+		point.x = p.x+Op.x;
+		point.y = p.y+Op.y;
+		return point;
+	}
+
+
+};
