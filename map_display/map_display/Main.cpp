6e3c1828a9b659ade2623bf300b185d0f7e3e0b6
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2022-12-11 11:46:48 +0900

    途中経過2

--- map_display/map_display/Main.cpp
+++ map_display/map_display/Main.cpp
@@ -1,9 +1,9 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.5
-#include"display_manager.h"
+//#include"display_manager.h"
 void Main()
 {
-	Screen_Resizer display({ 1200,1000 });
-	display.SetStyle(ScreenStyle::Center);
+	//Screen_Resizer display({ 1200,1000 });
+	//display.SetStyle(ScreenStyle::Center);
 	//Scene::SetResizeMode(ResizeMode::Keep);
 	//シーンサイズ固定 今回はこれは使わずにやる
 	Window::SetStyle(WindowStyle::Sizable);
@@ -12,10 +12,11 @@ void Main()
 
 	while (System::Update())
 	{
-		display.Resize_Update();
-		RectF{ display.Cal_Pos({0,0}), display.Cal_Size({1200,1000}) }.draw(Palette::Aqua);
-
+		//display.Resize_Update();
+		Print<<Scene::DeltaTime();
 		//display.toReal(RectF{ 0,0, 1200, 1000 }).draw(Palette::Green);
+		RectF{0,0, 400,400 }.draw(Palette::Aqua);
+
 		//display.toReal(Circle{ 1100,900,20 }).draw(Palette::Pink);
 		//display.toReal(Circle{ 600,500,20 }).draw(Palette::Pink);
 	}
