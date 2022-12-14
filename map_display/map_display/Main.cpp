5bb3a6b4a734b79de2a6f92ddf556f56f4a20874
ryo-0723 <98096559+ryo-0723@users.noreply.github.com>
2022-12-14 19:04:15 +0900

    リサイズ用の初期機能のクラスは完成した

--- map_display/map_display/Main.cpp
+++ map_display/map_display/Main.cpp
@@ -1,24 +1,46 @@
 ﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.5
-#include"Screen_Builder.h"
+#include"display_manager.h"
+SIV3D_SET(EngineOption::Renderer::OpenGL);
+
+std::pair<RectF, RectF> Screen_SplitFixedRight(const RectF& raw, double px) {
+	return std::make_pair(
+		RectF{ 0,0,raw.w - px,raw.h },
+		RectF{ raw.w-px,0,px,raw.h }
+	);
+}
+
 void Main()
 {
-	Screen_Resizer display({ 1200,1000 });
-	Screen_build display1({200,1000}, display);
-	display.SetStyle(ScreenStyle::Center);
-	//Scene::SetResizeMode(ResizeMode::Keep);
-	//シーンサイズ固定 今回はこれは使わずにやる
-	Window::SetStyle(WindowStyle::Sizable);
-	//ウィンドウを自由に大きさ変えれるやつ
-	//Rect{座標ｘ,座標y,大きさx,大きさy}.draw
+	Window::SetMinimumFrameBufferSize({ 800, 600 });
+	Window::Resize(1000, 1000);
+	Screen_Resizer display1({ 8000,7500 });
+	Screen_Resizer display2({ 700,500 }, ScreenStyle::Keep);
+	display1.SetStyle(ScreenStyle::Center);
+	Window::SetStyle(WindowStyle::Sizable);//ウィンドウを自由に大きさ変えれるやつ
 
 	while (System::Update())
 	{
-		display.Resize_Update();
+		const auto [left, right] = Screen_SplitFixedRight(Scene::Rect(), 600);
+		left.draw(ColorF{ 0.0, 0.5, 1.0, 0.2 });
+		right.draw(ColorF{ 1.0, 0, 0, 0.2 });
+
+		display1.Resize_Update(left);
+		display2.Resize_Update(right);
+		ClearPrint();
+		Print << display2.Get_Pos();
+		Print << Cursor::Pos();
+		Print << right.pos.x;
 		//Print<<Scene::DeltaTime();
-		display.toReal(RectF{ 0,0, 1200, 1000 }).draw(Palette::Green);
+		display1.toReal(RectF{ 0,0, 8000, 7500 }).draw(Palette::Green);
+		//display2.toReal(RectF{ 0,0, 700, 1000 }).draw(ColorF{ 0.0, 0.5, 1.0, 0.5 });
+
 		//RectF{0,0, 400,400 }.draw(Palette::Aqua);
 
-		//display.toReal(Circle{ 1100,900,20 }).draw(Palette::Pink);
-		//display.toReal(Circle{ 600,500,20 }).draw(Palette::Pink);
+		display1.toReal(Circle{ 8000,7500,100 }).draw(Palette::Pink);
+		display2.toReal(Circle{ 350,250,80 }).draw(Palette::Pink);
+		display1.toReal(Circle{ 4000,3750,100 }).draw(Palette::Pink);
+		display2.toReal(Circle{ 700,500,20 }).draw(Palette::Pink);
+		display2.toReal(Circle{ 0,0,20 }).draw(Palette::Pink);
+
 	}
 }
--- map_display/map_display/display_manager.h
+++ map_display/map_display/display_manager.h
@@ -11,37 +11,64 @@ OpenSiv3D用の画面の縮小拡大を自動的に行うもの
 class Screen_Resizer {
 private:
 	Vec2 Virtual_Size;
-	Vec2 Offset_Pos = {};
-	Vec2 Scalling = {};
-	double Scall = 0;
-	uint8 Style = 1;
+	ScreenStyle Style;
+	Vec2 Offset_Pos = {}, Scalling = {}, Scall = {};
+	RectF Screen = {};
+	int defaultKeepPosY;
 public:
-	Screen_Resizer(const Vec2& Virtual_Size) :Virtual_Size(Virtual_Size) {}
-	/*仮想スクリーンの大きさを指定する*/
+	Screen_Resizer(const Vec2& Virtual_Size, ScreenStyle Style = ScreenStyle::Center)
+		:Virtual_Size(Virtual_Size), Style(Style) {
+		defaultKeepPosY = Scene::Size().y;
+		//最初に表示されたy軸の大きさを最大として、スクリーンをリサイズする
+		//***これより先にウィンドウのの初期表示サイズを設定しておくこと
+	}
+	/*
+	 *仮想スクリーンの大きさを指定する
+	 *第二引数は画面タイプの設定を行う
+	*/
 
-	void Resize_Update() {
-		Scalling = Scene::Size() / Virtual_Size;
+	/// @brief 描画領域内に表示させるスクリーンのリサイズ
+	/// @param Screen 描画させたい領域
+	void Resize_Update(const RectF& Screen) {
+		this->Screen = Screen;
+		Scalling = Screen.size / Virtual_Size;
 		//仮想ウィンドウと実ウィンドウの大きさの比率を求める
-		Scall = Scalling.x < Scalling.y ? Scalling.x : Scalling.y;
-		//x,yのうち、より縮小しなければ画面に入らないほうの縮小率を求める
-		if (Style == 0) {
-			Offset_Pos.x = 0;
-			Offset_Pos.y = (Scene::Size().y - Virtual_Size.y * Scall) * 0.5;
-		}
-		else if (Style == 1) {
-			Offset_Pos = (Scene::Size() - Virtual_Size * Scall) * 0.5;
-		}
-		else {
-			Offset_Pos.x = (Scene::Size().x - Virtual_Size.x * Scall);
-			Offset_Pos.y = (Scene::Size().y - Virtual_Size.y * Scall) * 0.5;
+		switch (Style) {
+		case ScreenStyle::Left:
+			Scall.x = Scall.y = Scalling.x < Scalling.y ? Scalling.x : Scalling.y;
+			//x,yのうち、より縮小しなければ画面に入らないほうの縮小率を求める
+			Offset_Pos.x = Screen.pos.x;
+			Offset_Pos.y = Screen.pos.y + (Screen.size.y - Virtual_Size.y * Scall.y) * 0.5;
+			//左上の座標原点を調整する
+			break;
+		case ScreenStyle::Center:
+			Scall.x = Scall.y = Scalling.x < Scalling.y ? Scalling.x : Scalling.y;
+			Offset_Pos = Screen.pos + (Screen.size - Virtual_Size * Scall) * 0.5;
+			break;
+		case ScreenStyle::Right:
+			Scall.x = Scall.y = Scalling.x < Scalling.y ? Scalling.x : Scalling.y;
+			Offset_Pos.x = Screen.pos.x + (Screen.size.x - Virtual_Size.x * Scall.x);
+			Offset_Pos.y = Screen.pos.y + (Screen.size.y - Virtual_Size.y * Scall.y) * 0.5;
+			break;
+		case  ScreenStyle::Keep:
+			Scall.x = Scalling.x;
+			Scall.y = defaultKeepPosY / Virtual_Size.y;
+			Offset_Pos.x = Screen.pos.x;
+			Offset_Pos.y = Screen.pos.y;
+			break;
 		}
 		//pos*scallは実ウィンドウ内での仮想ウィンドウの大きさ
 	}
 	Vec2 GetWindow_Size() {
-		return Scene::Size() * Scall;
+		return Screen.size * Scall;
 	}
+	Vec2 Get_Pos() {
+		return Offset_Pos;
+	}
+	/// @brief ウィンドウの表示タイプの変更
+	/// @param Style 表示タイプ
 	void SetStyle(ScreenStyle Style) {
-		this->Style = (uint8)Style;
+		this->Style = Style;
 	}
 	/// @brief 実ウィンドウでの座標になるよう計算
 	/// @param Pos 仮想ウィンドウでの座標
@@ -56,7 +83,7 @@ public:
 	/// @brief 実ウィンドウでの大きさになるように計算
 	/// @param Size 仮想ウィンドウでの大きさ
 	double Cal_Size(const double& Size)const {
-		return Size * Scall;
+		return Size * Scall.x;
 	}
 
 	Line toReal(const Line& Raw) const {
