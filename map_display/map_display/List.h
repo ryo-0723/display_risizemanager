a8ca3df5f29edcf0f47dc59f9bd1f732812f6277
涼 藤本 <jlgifydhdhtr@outlook.jp>
2023-01-02 10:15:26 +0900

    途中経過

---
+++ map_display/map_display/List.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include"display_manager.h"
+
+class List {
+private:
+	Screen_Resizer& resizer;
+	bool slider = false;//スライダーの現在の位置
+public:
+
+	List(Screen_Resizer& resizer) :resizer(resizer) {}
+
+
+
+
+
+
+};
